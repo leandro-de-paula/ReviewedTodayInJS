dcedb5618e9c6a8fffb6620835b335a2c6dfc3cf
Leandro de Paula <leandrodepaula.ti@gmail.com>
2022-02-09 08:12:50 -0300

    revisto o conceito do var como variavel global, seu escopo global e escopo de função

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{ // bloco 1
+    { // bloco 2
+        { // bloco 3
+            { // bloco 4
+                var sera = 'Sera???' 
+            } 
+        } 
+    } 
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+// console.log(local) // erro - fora do escopo de funcao

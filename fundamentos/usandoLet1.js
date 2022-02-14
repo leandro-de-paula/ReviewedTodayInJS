f19b0a6206874730aa4c17fed10b8a494b676c24
leandro-de-paula <leandrodepaula.ti@gmail.com>
2022-02-14 15:40:58 -0300

    Variavel let e seus escopos

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+let numero = 1
+{
+    let numero = 2
+    console.log('dentro = ', numero)
+}
+
+console.log('fora =', numero)
+
+// let tem escopo global, função, e bloco -- um escopo a mais em relação ao var

d40fb5ec1c88c45d7dbb08b8782b8facbc49a22b
leandro-de-paula <leandrodepaula.ti@gmail.com>
2022-04-15 07:00:13 -0300

    Modificando a forma da execução do desafio, desmembrando do HTML, o CSS, e o JS, além de começar a tratar os elementos do video no HTML e CSS

---
+++ html/exercicios/videoDesafio.js
@@ -0,0 +1,17 @@
+
+function startVideoControl(id){
+    const video = document.getElementById(id)
+    video.oncontextmenu = () => false
+
+    document.querySelector(`[wm-play=${id}]`).onclick = e => video.play()
+    document.querySelector(`[wm-pause=${id}]`).onclick = e => video.pause()
+    document.querySelector(`[wm-stop=${id}]`).onclick = e => {
+        video.pause()
+        video.currentTime = 0}
+    }
+    // document.querySelector(`[wm-dec=${id}]`).onclick = e => video.playbackRate -= 0.10
+    // document.querySelector(`[wm-inc=${id}]`).onclick = e => video.playbackRate += 0.10
+    // document.querySelector(`[wm-retroceder=${id}]`).onclick = e => video.currentTime -= 0.10
+    // document.querySelector(`[wm-avancar=${id}]`).onclick = e => video.currentTime += 0.10
+
+startVideoControl("meuVideo")

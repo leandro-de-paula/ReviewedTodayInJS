c9728769183c68f18593a7f946b8c04249d1b4ad
leandro-de-paula <leandrodepaula.ti@gmail.com>
2022-06-17 04:25:38 -0300

    Ajax - XMLHttpRequest2 - Upload de arquivos

--- ajax/server.js
+++ ajax/server.js
@@ -5,8 +5,31 @@ const app = express()
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
 const now = new Date
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 
 app.get('/teste', (req, res) => res.send(
     'Executando... ' +  
